89296473433402c6df7f0041a22e0c71b4d5d3f6
Aditya Sankar <aditya.pyro@gmail.com>
2015-06-08 16:03:17 -0700

    Updates to Unity brushing demo

--- UnityVR/Assets/Plot/plot.cs
+++ UnityVR/Assets/Plot/plot.cs
@@ -24,7 +24,7 @@ public class DataPoint
 
 	public void deselect()
 	{
-		selected = false;
+		selected = false; //unselect
 		cube.renderer.material.color = Color.white;
 		sphere.renderer.material.color = Color.white;
 		cylinder.renderer.material.color = Color.white;
@@ -38,11 +38,13 @@ public class plot : MonoBehaviour {
 	public Color cz = Color.blue;
 
 	private GameObject centerCam;
-	private int num_points = 100;
+	private int num_points = 50;
 	private List<DataPoint> pointlist;
 	
 	// Use this for initialization
 	void Start () {
+
+		gameObject.renderer.material.color = Color.gray;
 
 		GameObject plot1 = new GameObject ("Plot1");
 		GameObject plot2 = new GameObject ("Plot2");
@@ -60,9 +62,13 @@ public class plot : MonoBehaviour {
 		setupPlot2 (plot2);
 		setupPlot3 (plot3);
 
-		plot1.transform.position = new Vector3 (0, 0, 2.0f);
-		plot2.transform.position = new Vector3 (-2, 0, 0.5f);
-		plot3.transform.position = new Vector3 (2.5f, 0, 0.5f);
+		plot1.transform.position = new Vector3 (0, 0.75f, 2.0f);
+		plot2.transform.rotation = Quaternion.Euler (0, -45, 0);
+		plot3.transform.rotation = Quaternion.Euler (0, 90, 0);
+
+		plot2.transform.position = new Vector3 (-2, 0.75f, 0.5f);
+		plot3.transform.position = new Vector3 (2.5f, 0.75f, 0.5f);
+
 
 		//
 		centerCam = GameObject.Find ("LeftEyeAnchor");
@@ -74,7 +80,7 @@ public class plot : MonoBehaviour {
 		for (int i=0; i< num_points; i++) {
 			GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
 			cube.transform.parent = plot.transform;
-			cube.transform.position = new Vector3 (Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-0.3f, 0.3f));
+			cube.transform.position = new Vector3 (Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-0.15f, 0.15f));
 			cube.transform.localScale = new Vector3 (0.1f, 0.1f, 0.1f);
 
 			pointlist[i].cube = cube;
@@ -118,8 +124,9 @@ public class plot : MonoBehaviour {
 		for (int i=0; i< num_points; i++) {
 			GameObject cylinder = GameObject.CreatePrimitive (PrimitiveType.Cylinder);
 			cylinder.transform.parent = plot.transform;
-			cylinder.transform.position = new Vector3 (Random.Range(-1.0f, 1.0f), -1.0f, Random.Range(-1.0f, 1.0f));
-			cylinder.transform.localScale = new Vector3 (0.1f, Random.Range(0.0f, 1.2f), 0.1f);
+			float height = Random.Range(0.0f, 0.2f);
+			cylinder.transform.localScale = new Vector3 (0.1f, height, 0.1f);
+			cylinder.transform.position = new Vector3 (Random.Range(-1.0f, 1.0f), height - 1.0f, Random.Range(-1.0f, 1.0f));
 			
 			pointlist[i].cylinder = cylinder;
 		}
@@ -168,8 +175,8 @@ public class plot : MonoBehaviour {
 				GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 				sphere.transform.parent = plot.transform;
 
-				float xt = x * 2.0f/10.0f -1;
-				float zt = z * 2.0f/10.0f -1;
+				float xt = Random.Range(-1.0f, 1.0f);//x * 2.0f/10.0f -1;
+				float zt = Random.Range(-1.0f, 1.0f);//z * 2.0f/10.0f -1;
 
 				float y = 1.5f * Mathf.Exp(-(Mathf.Pow(xt, 2.0f) + Mathf.Pow(zt, 2.0f)/(2.0f*sx*sx)));
 
